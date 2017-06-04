84587224ce7de6c9ff1c80fef7db551bc38b0b19
Edward Ransome <edward.ransome@heig-vd.ch>
2017-06-04 13:56:42 +0000

    aldo a Docker image wrapping an express.js that return random array of students

--- express-image/src/index.js
+++ express-image/src/index.js
@@ -4,13 +4,9 @@ var express = require('express');
 var app = express();
 
 
-app.get('/test', function (req, res) {
-  res.send('hey aldo testerino!');
-})
-
 
 app.get('/', function (req, res) {
-  res.send('hey aldo!');
+  res.send( generateStudents() );
 })
 
 
@@ -18,3 +14,31 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('Accept HTTP requests on port 3000.');
 })
+
+function generateStudents(){
+	var numberOfStudents = chance.integer({
+		min: 0,
+		max: 10
+	});
+	console.log(numberOfStudents);
+	var students = [];
+	for(var i = 0; i < numberOfStudents; i++) {
+		var gender = chance.gender();
+		var birthYear = chance.year({
+		min: 1986,
+		max: 1996
+		});
+		students.push({
+			firstName: chance.first({
+				gender: gender
+			}),
+			lastName: chance.last(),
+			gender: gender,
+			birthday: chance.birthday({
+				year: birthYear
+			})
+		})
+	};
+	console.log(students);
+	return students;
+}
